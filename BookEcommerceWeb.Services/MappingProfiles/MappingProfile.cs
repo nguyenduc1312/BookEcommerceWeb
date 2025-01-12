403cad56950db0cf5160b7294999331ec7d317f4
duc_99 <nguyenduc1312.pt@gmail.com>
2025-01-13 00:40:55 +0700

    add mapping for Product

--- BookEcommerceWeb.Services/MappingProfiles/MappingProfile.cs
+++ BookEcommerceWeb.Services/MappingProfiles/MappingProfile.cs
@@ -18,6 +18,18 @@ namespace BookEcommerceWeb.Services.MappingProfiles
             .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
             .ForMember(dest => dest.DisplayOrder, opt => opt.MapFrom(src => src.DisplayOrder))
             .ReverseMap();
+
+            CreateMap<Product, ProductDto>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))                  
+            .ForMember(dest => dest.Title, opt => opt.MapFrom(src => src.Title))            
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description))
+            .ForMember(dest => dest.Author, opt => opt.MapFrom(src => src.Author))          
+            .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price))            
+            .ForMember(dest => dest.Price50, opt => opt.MapFrom(src => src.Price50))        
+            .ForMember(dest => dest.Price100, opt => opt.MapFrom(src => src.Price100))      
+            .ForMember(dest => dest.CategoryId, opt => opt.MapFrom(src => src.CategoryId))  
+            .ForMember(dest => dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl))
+            .ReverseMap(); ;     
         }
     }
 }
